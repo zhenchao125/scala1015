8523b3f6b6b3c5e09b6b672a1cca4065cc8fa3d7
John Doe <30752740@qq.com>
2020-03-02 14:22:32 +0800

    更新

--- src/main/scala/com/atguigu/scalal1015/day01/String_1.scala
+++ src/main/scala/com/atguigu/scalal1015/day01/String_1.scala
@@ -6,8 +6,36 @@ package com.atguigu.scalal1015.day01
  */
 object String_1 {
     def main(args: Array[String]): Unit = {
-    
-    
+        /*val a = 20
+        
+        printf("格式化输出 %d", a)
+        
+        printf("浮点数: %f\n", math.Pi)
+        printf("浮点数: %.2f\n", math.Pi)
+        printf("字符串: %s, %f, %d", "abc", 1.2, 1)*/
+        val a = 10
+        val b = 20
+        // "a = 10, b = 20"
+//        val s = "a = " + a + ", b = " + b
+        // s插值
+//        val s = s"a = $a, b = $b"
+        //val s = s"a = ${a*2}, b = ${b*3}"
+        // raw 插值
+        val s = raw"\n \t \s"
+        println(s)
     
     }
 }
+/*
+1. java的输出
+    System.out.println("")
+2. scala的输出
+    println("")
+3. printf
+    格式化输出
+        %d 输出整数
+        %s 输出字符串
+        %f 输出浮点数
+4. 字符串插值
+ 
+ */
---
+++ src/main/scala/com/atguigu/scalal1015/day01/String_2.scala
@@ -0,0 +1,41 @@
+package com.atguigu.scalal1015.day01
+
+/**
+ * Author atguigu
+ * Date 2020/3/2 14:15
+ */
+object String_2 {
+    def main(args: Array[String]): Unit = {
+        /*val s =
+            """你阿法拉圣诞节疯狂拉萨的
+爱上了房间爱死了附近
+精灵盛典荆防颗粒
+               
+aaaa
+               aq
+              """*/
+        /*val s =
+            """
+              |afaf
+              |aaa
+              |bb
+              |ccc
+              |""".stripMargin*/
+        val date = "2020-03-02"
+        val s =
+        s"""
+          |select
+          | *
+          |from user
+          |where date='$date'
+          |""".stripMargin
+        println(s)
+    }
+}
+
+/*
+多行字符串:
+ 
+
+
+ */
