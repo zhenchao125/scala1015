40008cb7dfd7bd78f371b1d444b456336732ec70
John Doe <30752740@qq.com>
2020-03-03 15:43:51 +0800

    更新

--- src/main/scala/com/atguigu/scalal1015/day02/high/High5.scala
+++ src/main/scala/com/atguigu/scalal1015/day02/high/High5.scala
@@ -5,18 +5,37 @@ package com.atguigu.scalal1015.day02.high
  * Date 2020/3/3 15:13
  *
  * 自动格式化:   alt+ctrl+L
+ * 复制一行代码: ctrl+D
  */
 object High5 {
     def main(args: Array[String]): Unit = {
-        val arr1 = Array(30, 50, 70, 60, 10, 20)
+        val arr1 = Array(30, 5, 7, 60, 1, 20)
+        
         /*foreach(arr1, a => {
             println(a)
         })*/
-        foreach(arr1, println)
+        //        foreach(arr1, println)
+//        val arr2 = filter(arr1, x => x % 2 == 0)
+//        val arr2 = filter(arr1, _ % 2 == 0)
+//        val arr2 = filter(arr1, x => x > 25)
+        val arr2 = filter(arr1, _ > 25)
+        foreach(arr2, println)
     }
     
     /**
+     * 使用给定的条件对数组进行过滤
+     * @param arr
+     * @param condition  过滤条件, 返回时true留下
+     */
+    def filter(arr: Array[Int], condition: Int => Boolean) = {
+        // 通过一个数组, 得到一个新的数组   for推导
+        for (elem <- arr if condition(elem)) yield elem
+    }
+    
+    
+    /**
      * 用来遍历数组的高阶函数
+     *
      * @param arr
      * @param op
      */
@@ -35,6 +54,7 @@ foreach
     具体遍历到每个元素的时候, 做什么事情, 那是有调用者来确定
 
 filter
+    过滤.  过滤条件可以通过一个返回boolean的函数来体现
 
 map
 
