aa9e79a2f61bda98f3ed28917bf0f6fe1f9c85f2
John Doe <30752740@qq.com>
2020-03-03 10:20:45 +0800

    更新

---
+++ src/main/scala/com/atguigu/scalal1015/day02/fun/For4.scala
@@ -0,0 +1,23 @@
+package com.atguigu.scalal1015.day02.fun
+
+/**
+ * Author atguigu
+ * Date 2020/3/3 9:55
+ */
+object For4 {
+    def main(args: Array[String]): Unit = {
+        // 9*9乘法表
+        /*for(i <- 1 to 9){
+            for(j <- 1 to i){
+                print(s"$j * $i = ${i * j}\t")
+            }
+            println()
+        }*/
+        // 所有的代码都在内循环, 则可以使用循环的嵌套
+        // 实际情况使用很少
+        for(i <- 1 to 9;  j <- 1 to i){
+            print(s"$j * $i = ${i * j}\t")
+            if(i == j) println()
+        }
+    }
+}
---
+++ src/main/scala/com/atguigu/scalal1015/day02/fun/For5.scala
@@ -0,0 +1,24 @@
+package com.atguigu.scalal1015.day02.fun
+
+/**
+ * Author atguigu
+ * Date 2020/3/3 10:10
+ */
+object For5 {
+    def main(args: Array[String]): Unit = {
+        // 得到一个序列: 1 4 9 16 ... 100*100
+        // for推导式
+        /*val r = for (i <- 1 to 100) yield i * i
+        println(r)*/
+        
+        // "abcd" => "AaBbCc"
+        val s = for(c <- "abcd") yield c.toString.toUpperCase + c
+        println(s.mkString(""))
+        // 更加函数式
+        println("abcd".map(c => c.toString.toUpperCase + c).mkString(""))
+    }
+}
+
+/*
+循环没有返回值(Unit)
+ */
