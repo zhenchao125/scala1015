9968568ae4640cc4fd40a455d8b7a86c2995a98c
John Doe <30752740@qq.com>
2020-03-03 16:31:17 +0800

    更新

--- src/main/scala/com/atguigu/scalal1015/day02/high/High5.scala
+++ src/main/scala/com/atguigu/scalal1015/day02/high/High5.scala
@@ -10,24 +10,52 @@ package com.atguigu.scalal1015.day02.high
 object High5 {
     def main(args: Array[String]): Unit = {
         val arr1 = Array(30, 5, 7, 60, 1, 20)
+        println(arr1.sum)
+    
+        /*val result: Int = reduce(arr1, (x, y) => {
+            println(s"x = $x, y = $y")
+            x * y
+        })*/
+        val r: Int = reduce(arr1, _ * _)
+        println(r)
         
         /*foreach(arr1, a => {
             println(a)
         })*/
         //        foreach(arr1, println)
-//        val arr2 = filter(arr1, x => x % 2 == 0)
-//        val arr2 = filter(arr1, _ % 2 == 0)
-//        val arr2 = filter(arr1, x => x > 25)
-//        val arr2 = filter(arr1, _ > 25)
+        //        val arr2 = filter(arr1, x => x % 2 == 0)
+        //        val arr2 = filter(arr1, _ % 2 == 0)
+        //        val arr2 = filter(arr1, x => x > 25)
+        //        val arr2 = filter(arr1, _ > 25)
         
-//        val arr2 = map(arr1, x => x * x * x)
-//        foreach(arr2, println)
+        //        val arr2 = map(arr1, x => x * x * x)
+        //        foreach(arr2, println)
+        
         
         
     }
     
     /**
+     * 聚合查找
+     * 1 2 3 4 5 6..100
+     * op: (c1, c2) => r
+     * c1表示上次聚合的结果, c2这次参与聚合的元素
+     *
+     * @param arr
+     *
+     */
+    def reduce(arr: Array[Int], op: (Int, Int) => Int): Int = {
+        var lastReduce = arr(0)
+        for(i <- 1 until arr.length){
+            lastReduce = op(lastReduce, arr(i))
+        }
+        lastReduce
+    }
+    
+    
+    /**
      * map操作
+     *
      * @param arr
      * @param op
      * @return
@@ -39,8 +67,9 @@ object High5 {
     
     /**
      * 使用给定的条件对数组进行过滤
+     *
      * @param arr
-     * @param condition  过滤条件, 返回是true留下
+     * @param condition 过滤条件, 返回是true留下
      */
     def filter(arr: Array[Int], condition: Int => Boolean) = {
         // 通过一个数组, 得到一个新的数组   for推导
@@ -75,6 +104,11 @@ map
     映射. 一进一出
 
 reduce
+    聚合操作
+    
+    
+ 
+ 
 
 
  */
