b31453c536149b38e5e25276ce7e0205e0ec99be
John Doe <30752740@qq.com>
2020-03-09 11:37:43 +0800

    更新

---
+++ src/main/scala/com/atguigu/scalal1015/day06/Streamdemo/Stream1.scala
@@ -0,0 +1,11 @@
+package com.atguigu.scalal1015.day06.Streamdemo
+
+/**
+ * Author atguigu
+ * Date 2020/3/9 11:37
+ */
+object Stream1 {
+    def main(args: Array[String]): Unit = {
+        
+    }
+}
