cb3a5fbbf8dff03c544b096ded81789d02d10384
John Doe <30752740@qq.com>
2020-03-09 10:52:40 +0800

    更新

---
+++ src/main/scala/com/atguigu/scalal1015/day06/sort/Sort2.scala
@@ -0,0 +1,29 @@
+package com.atguigu.scalal1015.day06.sort
+
+/**
+ * Author atguigu
+ * Date 2020/3/9 10:51
+ */
+object Sort2 {
+    def main(args: Array[String]): Unit = {
+        
+    }
+}
+/*
+scala:
+    不管是可变集合还是不可变集合, 都是返回一个新的排好的序集合
+    原集合不动
+sorted
+    默认都是自然排序(升序)
+    
+    1. 让排序的类型有自己排序的功能
+        Ordered  就是java中的 Comparable
+    
+    2. 找一个第3方的比较器
+        Ordering 就是java中的 Comparator
+
+sortBy
+
+
+sortWith
+ */
