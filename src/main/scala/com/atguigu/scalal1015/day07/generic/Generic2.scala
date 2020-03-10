e9751327bc69fdbbefbf50c5207b337b8d6cceae
John Doe <30752740@qq.com>
2020-03-10 12:03:14 +0800

    更新

--- src/main/scala/com/atguigu/scalal1015/day07/generic/Generic2.scala
+++ src/main/scala/com/atguigu/scalal1015/day07/generic/Generic2.scala
@@ -9,9 +9,9 @@ case class AA(val age: Int) extends Ordered[AA]{
 }
 object Generic2 {
     def main(args: Array[String]): Unit = {
-//        println("a" < "b")
-//        val aa = compare(new AA(10), new AA(20))
-//        println(aa)
+        println("a" < "b")
+        val aa = compare(new AA(10), new AA(20))
+        println(aa)
         
         val max: Int = compare(10, 20)
         println(max)
