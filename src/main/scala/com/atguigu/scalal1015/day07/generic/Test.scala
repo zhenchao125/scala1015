554af6a291929296bfddabfd699191e6dd05495b
John Doe <30752740@qq.com>
2020-03-10 15:43:56 +0800

    更新

--- src/main/scala/com/atguigu/scalal1015/day07/generic/Test.scala
+++ src/main/scala/com/atguigu/scalal1015/day07/generic/Test.scala
@@ -6,15 +6,17 @@ package com.atguigu.scalal1015.day07.generic
  */
 object Test {
     def main(args: Array[String]): Unit = {
-        println(mkString(List(1, 2, 3), "<<", ",", ">>"))
-        println(mkString(List("a", "b", "d"), "<<", ",", ">>"))
-        println(mkString(Array("a", "b", "d"), "<<", ",", ">>"))
-        println(mkString(Set("a", "b", "d"), "<<", ",", ">>"))
+        println(mkString(List(1, 20, 3), "<<", ",", ">>"))
+        println(mkString(List("a", "c", "b"), "<<", ",", ">>"))
+//        println(mkString(List("a", "b", "d"), "<<", ",", ">>"))
+//        println(mkString(Array("a", "b", "d"), "<<", ",", ">>"))
+//        println(mkString(Set("a", "b", "d"), "<<", ",", ">>"))
     }
     
-    def mkString[T](list: Iterable[T], start: String, sep: String, end: String) = {
+    def mkString[T <% Ordered[T]](list: Iterable[T], start: String, sep: String, end: String) = {
         
-        val mid = list.foldLeft("")(_ + sep + _).substring(sep.length)
-        s"$start${mid}$end"
+        /*val mid = list.foldLeft("")(_ + sep + _).substring(sep.length)
+        s"$start${mid}$end"*/
+        list.reduce((x,y) => if(x > y) x else y)
     }
 }
